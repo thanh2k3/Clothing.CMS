911563c064276c126d2280cbb06c88312cea0b71
Trần Văn Thanh <tranvanthanh.12012003@gmail.com>
2024-07-10 14:02:37 +0700

    feat: add login admin interface

---
+++ Clothing.CMS.Web/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clothing.CMS.Web.Areas.Admin.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
