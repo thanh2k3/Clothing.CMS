5eb712b2d82681969601229c6e68dc9818e2a1bc
Trần Văn Thanh <tranvanthanh.12012003@gmail.com>
2024-07-11 23:37:35 +0700

    feat: add identity

---
+++ Clothing.CMS.EntityFrameworkCore/Pattern/CMSDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clothing.CMS.EntityFrameworkCore.Pattern
+{
+	public class CMSDbContext : IdentityDbContext<CMSIdentityUser>
+	{
+		public CMSDbContext() { }
+
+		public CMSDbContext(DbContextOptions options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			//Các bảng mặc định của CMS
+			CMSModuleBuilder.RegisterModule(builder);
+		}
+	}
+}
--- Clothing.CMS.Web/Program.cs
+++ Clothing.CMS.Web/Program.cs
@@ -1,4 +1,17 @@
+using Clothing.CMS.EntityFrameworkCore.Pattern;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// For Entity Framework
+var connectionString = builder.Configuration["ConnectionStrings:Database"];
+builder.Services.AddDbContext<CMSDbContext>(o => o.UseSqlServer(connectionString, sqlServerOptionsAction: sqlOption => { sqlOption.EnableRetryOnFailure(); }));
+
+//Identity
+builder.Services.AddIdentity<CMSIdentityUser, IdentityRole>()
+				.AddEntityFrameworkStores<CMSDbContext>()
+				.AddDefaultTokenProviders();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
