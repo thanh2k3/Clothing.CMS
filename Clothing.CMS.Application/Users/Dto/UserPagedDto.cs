9793eb0ce182e2d24c3730329fb20542997cfa19
Trần Văn Thanh <tranvanthanh.12012003@gmail.com>
2024-07-23 00:33:48 +0700

    feat: add paged

---
+++ Clothing.CMS.Application/Users/Dto/UserPagedDto.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Clothing.CMS.Application.Common.Dto;
+using Clothing.CMS.EntityFrameworkCore.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothing.CMS.Application.Users.Dto
+{
+    public class UserPagedRequestDto : PagedRequestDto
+    {
+    }
+}
