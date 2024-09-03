869adbe28d6837ab26a6bd609565ac12abdf8813
Trần Văn Thanh <tranvanthanh.12012003@gmail.com>
2024-09-03 08:38:21 +0700

    feat: add logevent table

---
+++ Clothing.CMS.Entities/LogEvent.cs
@@ -0,0 +1,14 @@
+﻿using Clothing.CMS.Entities.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clothing.CMS.Entities
+{
+	[Table("LogEvent")]
+	public class LogEvent : BaseEntity
+	{
+		public string LogLevel { get; set; }
+		public string Message { get; set; }
+		public string Values { get; set; }
+		public DateTime CreatedTime { get; set; }
+	}
+}
---
+++ Clothing.CMS.EntityFrameworkCore/Migrations/20240903013537_Add_LogEvent_Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clothing.CMS.EntityFrameworkCore.Migrations
+{
+    public partial class Add_LogEvent_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogEvent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LogLevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Values = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogEvent", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogEvent");
+        }
+    }
+}
