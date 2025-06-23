78a7bfe862634b826bfb35debfeb468d60e37535
thanh2k3 <tranvanthanh.12012003@gmail.com>
2025-06-23 15:06:05 +0700

    feat: edit login page

---
+++ Clothing.CMS.Application/Common/Dto/BaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Clothing.CMS.Application.Common.Dto
+{
+    public class BaseResponse<T>
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public T? Data { get; set; }
+    }
+}
---
+++ Clothing.CMS.Web/Areas/Admin/ViewModels/Auth/LoginVMMapProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Clothing.CMS.Application.Auths.Dto;
+
+namespace Clothing.CMS.Web.Areas.Admin.ViewModels.Auth
+{
+	public class LoginVMMapProfile : Profile
+	{
+		public LoginVMMapProfile()
+		{
+			CreateMap<LoginViewModel, LoginDto>();
+		}
+	}
+}
